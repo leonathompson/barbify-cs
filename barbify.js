20cbb9cd1b26be8976114ce44c70cc4564c4d05d
Martiece Thompson <supsup@Leonas-MacBook-Pro.local>
2023-08-09 21:33:14 -0400

    created extension

---
+++ barbify.js
@@ -0,0 +1,12 @@
+// update logo to Barbified CS logo 
+
+const logo = document.querySelector("#hs-link-site_logo_hs_logo_widget > img");
+logo.src = '/images/Barbie.png';
+
+// update hero background 
+
+const background = document.querySelector("#hs_cos_wrapper_widget_1657116293820 > section > div.section-background-color");
+background.style.background = "linear-gradient(179.78deg, #FFFFFF 33.41%, #FD0086 84.81%";
+
+// change the dots to pink 
+const svg1 = document.querySelector("#hs_cos_wrapper_widget_1657116293820 > section > div.content-wrapper > div > div.img-block > div.top-shape > svg");
